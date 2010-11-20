120bdc0b2863fd9e47df4c7613af3a5ec4c28b3e
Alex Pearce <alex@alexpearce.me>
2010-11-20 19:18:51 +0000

    More styling, fancybox.

--- app/controllers/baskets_controller.rb
+++ app/controllers/baskets_controller.rb
@@ -2,6 +2,10 @@ class BasketsController < ApplicationController
   respond_to :html
   
   def show
-    respond_with @basket
+    if @basket.line_items.count == 0
+      redirect_to items_path, :notice => 'You do not have any items in your basket.'
+    else
+      respond_with @basket
+    end
   end
 end
