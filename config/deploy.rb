eb94a3452438d59136d9ec74161f0f77fc3d0b7e
Alex Pearce <alex@alexpearce.me>
2011-04-11 00:22:27 +0100

    Require Cap bundler stuff so gems get installed.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+require "bundler/capistrano"
+
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_ruby_string, '1.9.2@store'        # Or whatever env you want it to run in.
