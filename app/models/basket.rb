1382395ba5ac8c30f8aec657c573d97929da5769
Alex Pearce <alex@alexpearce.me>
2011-04-22 12:01:38 +0100

    Added a PayPal method to the basket. Sandbox PayPal checkout working.

--- app/models/basket.rb
+++ app/models/basket.rb
@@ -11,4 +11,24 @@ class Basket < ActiveRecord::Base
       li.item.postage * 1.2**(li.quantity - 1)
     }
   end
+  
+  def paypal_url(return_url)
+    values = {  
+      :business => 'paypal_1303466866_biz@alexpearce.me',
+      :cmd => '_cart',
+      :upload => 1,
+      :return => return_url,
+      :invoice => id
+    }
+    
+    line_items.each_with_index do |li, index|
+      values.merge!({
+        "amount_#{index + 1}" => li.item.price,
+        "item_name_#{index + 1}" => li.item.self_with_parent,
+        "item_number_#{index + 1}" => li.item.id,
+        "quantity_#{index + 1}" => li.quantity
+      })
+    end 
+    "https://www.sandbox.paypal.com/cgi-bin/webscr?#{values.to_query}"
+  end
 end
